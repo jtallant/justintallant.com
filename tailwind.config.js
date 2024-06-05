288709b827db5eca25b9af7ec7e50b8968f56a01
Justin Tallant <jtallant07@gmail.com>
2024-06-04 21:25:13 -0600

    css updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,15 @@ module.exports = {
     './site/templates/layouts/*.twig'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue',
+          'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji',
+          'Segoe UI Symbol', 'Noto Color Emoji',
+        ],
+      }
+    }
   },
   plugins: [],
 }
-
-
